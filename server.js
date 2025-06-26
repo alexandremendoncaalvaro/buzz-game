210491e52eae06ec2cb27bb3462f77da8cff79d8
Alê AmbevTech <alexandre.alvaro@ambevtech.com.br>
2025-06-26 23:17:47 +0000

    feat: Add Docker Compose configuration for the buzz-game service fix: Redirect to create.html on invalid game token in index.html

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const io = new Server(server);
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.redirect("/create.html");
+});
+
 const gameRooms = new Map();
 
 class GameRoom {
